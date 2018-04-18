eac3586f4401e8d870fc14202232eb583c9cad11
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-18 12:58:53 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,17 +1,21 @@
 class CouponsController < ApplicationController
 
   def index
+    bybug
     @coupons = Coupon.all
   end
 
   def show
+    byebug
     @coupon = Coupon.find(params[:id])
   end
 
   def new
+    bybug
   end
 
   def create
+    byebug
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
