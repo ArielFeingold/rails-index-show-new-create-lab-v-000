7c4598e2278ac937310c5c16545424fd98919967
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-18 12:36:58 +0000

    automatically backed up by learn

--- app/controllers/coupon_controller_controller.rb
+++ app/controllers/coupon_controller_controller.rb
@@ -1,6 +1,17 @@
 class CouponControllerController < ApplicationController
 
+  def index
+  end
+
   def new
   end
-  
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
 end
